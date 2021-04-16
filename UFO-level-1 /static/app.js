46469120f2d78d23750731258ba5e2e6275032ed
dosterman09 <72676969+dosterman09@users.noreply.github.com>
2021-04-15 22:17:26 -0500

    final push

---
+++ UFO-level-1 /static/app.js
@@ -0,0 +1,53 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+// Use D3 to select table
+var tbody = d3.select("tbody");
+console.log(data);
+function buildTable(ufosightings){
+    tbody.html("");
+    ufosightings.forEach(function(UFO) {
+//   console.log(UFO);
+  var row = tbody.append("tr");
+  Object.entries(UFO).forEach(function([key, value]) {
+    
+    var cell = row.append("td");
+    cell.text(value);
+  }                            );
+});
+}
+
+//  button
+var button = d3.select("#button");
+
+//  form
+var form = d3.select("#form");
+
+d3.selectAll("#filter-btn").on("click", runEnter);
+
+
+function runEnter() {
+  d3.event.preventDefault();
+  // HTML node input element
+  var inputElement = d3.select("#datetime");
+  // input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+//   date
+  let filteredData = tableData
+  if (inputValue) {
+    
+    filteredData = filteredData.filter(row => row.datetime === inputValue);
+  };
+
+//   filteredData
+  console.log(filteredData);
+  buildTable(filteredData);
+  
+};
+buildTable(tableData);
+
